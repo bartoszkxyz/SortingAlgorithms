7d3579fee0e53c7b3c991070bd777163568967f4
bartoszkxyz <pzx108025@student.wsb.poznan.pl>
2022-05-28 14:58:16 +0200

    changed Program to Driver with it's contents

---
+++ Sortowanie/Driver.cs
@@ -0,0 +1,26 @@
+﻿namespace Sortowanie
+{
+    internal class Driver
+    {
+        //declaration of rng
+        static Random rnd = new Random(Guid.NewGuid().GetHashCode());
+        
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Metoda \t 50k \t 60k \t 70k \t 80k \t 90k \t 100k \t 110k \t 120k \t 130k \t 140k \t 150k \t 160k \t 170k \t 180k \t 190k \t 200k");
+            for (int i = 50000; i < 200000; i+=10000)
+            {
+                int[] NumbersToSort = new int[i];
+                Generators.GenerateRandomArray(NumbersToSort, rnd);
+                //start pomiaru czasu
+                var watch = System.Diagnostics.Stopwatch.StartNew();
+                Algorithms.InsertionSort(NumbersToSort);
+                watch.Stop();
+                Console.Write(watch.ElapsedMilliseconds.ToString() + " \t ");
+                //stop pomiaru czasu
+            }
+
+        }
+    }
+}
